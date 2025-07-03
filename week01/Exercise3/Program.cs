4e2122c6706b63a20ecceaf4efb1ef49a5f85ae1
Geoffrey Kofi Etu <geoffreyetu56@gmail.com>
2025-07-03 11:54:06 +0000

    Finished Exercise3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,54 @@
 using System;
+using System.Data;
+using System.Globalization;
+using System.Net;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        string response = "";
+
+        do
+        {
+            //Getting a random number from the computer
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 100);
+
+
+            int number = 0;
+            int count = 0;
+
+            while (magicNumber != number)
+            {
+                Console.Write("Guess the number: ");
+                string guess = Console.ReadLine();
+                number = int.Parse(guess);
+                //To count the number of guesses the user made
+                count += 1;
+
+                if (number < magicNumber)
+                {
+                    Console.WriteLine("Guess higer");
+                }
+                else if (number > magicNumber)
+                {
+                    Console.WriteLine("Guess lower");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it");
+                    break;
+                }
+            }
+            Console.WriteLine($"You guessed: {count} times.");
+            Console.Write("Do you want to play again? ");
+            response = Console.ReadLine().ToLower();
+
+
+        } while (response == "yes");
+        
+       
+
     }
 }
