957846d24e064ba873bab08d1fef5c77e6b3634d
unknown <geoffreyetu56@gmail.com>
2025-07-29 17:43:59 +0000

    Inheritance homework completed

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -1,9 +1,25 @@
 using System;
+using System.Data;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment assignment = new Assignment("Samuel Bennet", "Multiplication");
+        string summary = assignment.GetSummary();
+        Console.WriteLine(summary);
+
+        MathAssignment assignment1 = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "9-18");
+        string summary1 = assignment1.GetSummary();
+        string homeWorkList = assignment1.GetHomeWorkList();
+        Console.WriteLine(summary1);
+        Console.WriteLine(homeWorkList);
+
+        WritingAssignment assignment2 = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        string summary2 = assignment2.GetSummary();
+        string getInfo = assignment2.GetWritingInformation();
+        Console.WriteLine(summary2);
+        Console.WriteLine(getInfo);
+
     }
 }
